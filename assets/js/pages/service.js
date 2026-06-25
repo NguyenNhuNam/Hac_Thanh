7b9557305b8a901554af88d91a385abe3c6dd8e1
unknown <nguyennhunam931@gmail.com>
2026-06-25 22:15:38 +0700

    update 25/06

--- assets/js/pages/service.js
+++ assets/js/pages/service.js
@@ -1,30 +1,72 @@
-window.addEventListener('load', () => {
-    AOS.refreshHard();
+AOS.init({ 
+    once: true, 
+    duration: 800
 });
 
-AOS.init({ once: true, duration: 500, offset: 200 });
+if ("scrollRestoration" in history) {
+    history.scrollRestoration = "manual";
+}
+
+const lenis = new Lenis({
+    lerp: 0.05,
+    wheelMultiplier: 1,
+});
+
+
+function resetScroll() {
+    lenis.scrollTo(0, {
+        immediate: true, 
+        force: true     
+    });
+
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            ScrollTrigger.refresh();
+            AOS.refreshHard(); 
+        });
+    });
+}
+
+window.addEventListener("load", resetScroll);
+window.addEventListener("pageshow", resetScroll);
+
+function raf(time) {
+    lenis.raf(time);
+    ScrollTrigger.update(); 
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+lenis.on("scroll", ScrollTrigger.update);
 
 gsap.to(".marquee-track", {
     xPercent: -50,
     duration: 20,
     ease: "none",
-    repeat: 1
+    repeat: -1, 
+    modifiers: {
+        xPercent: gsap.utils.unitize(x => parseFloat(x) % 50)
+    }
 });
 
 document.querySelectorAll('.category-swiper').forEach((el) => {
-    new Swiper(el, {
-        freeMode: true,
-        loop: true,
-        autoplay: {
-            delay: 0,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: false,
-        },
+    const nextButton = el.querySelector('.custom-swiper-next');
 
-        allowTouchMove: true,
-        grabCursor: true,
-        speed: 3000,
+    new Swiper(el, {
         slidesPerView: 'auto',
         spaceBetween: 8,
+        loop: true,
+        observer: true,      
+        observeParents: true, 
+        freeMode: {
+            enabled: true,
+            momentumRatio: 0.8,
+            momentumVelocityRatio: 0.8,
+            sticky: false,
+        },
+        grabCursor: true,
+        navigation: {
+            nextEl: nextButton,
+        }
     });
 });
